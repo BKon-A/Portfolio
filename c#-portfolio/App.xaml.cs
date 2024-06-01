32053e2b9ab4016ca6103b334ab32c2e243d11de
TheBlubby <supervit111@gmail.com>
2024-06-01 19:05:14 +0300

    Add project files.

---
+++ c#-portfolio/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace c__portfolio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
