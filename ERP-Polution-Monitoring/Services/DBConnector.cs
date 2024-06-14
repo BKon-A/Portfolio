95ed16425cd5043d4690872c08afaf2c8a409a32
BKon-A <bogdankonovalenko291@gmail.com>
2024-06-14 21:22:04 +0300

    Folders restructure. README update

---
+++ ERP-Polution-Monitoring/Services/DBConnector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using Npgsql;
+
+namespace DB_Reports_TPD_Lab_6
+{
+    public class DBConnector
+    {
+        public NpgsqlConnection Connection { get; set; }
+        string Host { get; } = "localhost";
+        int Port { get; set; } = 5432;
+        string Username { get; set; }
+        string Password { get; set; }
+        string Database { get; } = "BiospherePollutionResearch";
+
+        public DBConnector(string username, string password)
+        {
+            Username = username;
+            Password = password;
+
+            Connection = new NpgsqlConnection(GetConnectionString());
+        }
+
+        public NpgsqlConnection ConnectionOpen()
+        {
+            Connection.Open();
+            return Connection;
+        }
+        public NpgsqlConnection ConnectionClose()
+        { 
+            Connection.Close();
+            return Connection;
+        }
+
+        public string GetConnectionString()
+        {
+            return $"Host={Host};Port={Port};Username={Username};Password={Password};Database={Database}";
+        }
+    }
+}
